639ad844d85ff0a73db09ebc0eedb5437f567b47
Josh <sykopomp@sykosomatic.org>
2009-09-15 00:12:34 -0500

    LOGIC FTW

--- csp.lisp
+++ csp.lisp
@@ -248,7 +248,7 @@ new thread's name."
 ;; the receiver removes some from the channel and the sender
 ;; gets to put some in.
 (defun altcopy (sender receiver)
-  (when (not (or sender receiver))
+  (unless (or sender receiver)
     (return-from altcopy))
   (assert (not (null sender)))
   (let ((channel (alt-channel sender)))
