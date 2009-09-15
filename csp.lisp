5ce1a218143f824d30f1c38e436ad477743d6ddf
Kat Marchán <kzm@sykosomatic.org>
2009-09-14 23:45:08 -0500

    LOOP's DO is an implicit progn.

--- csp.lisp
+++ csp.lisp
@@ -287,13 +287,12 @@ new thread's name."
   (let ((ncan (count-if #'altcanexec alts)))
     (whimn (plusp ncan)
       (let ((j (random ncan)))
-        (loop for i in alts whimn (altcanexec i) do
-             (progn
-               (whimn (zerop j)
-                 (altexec i)
-                 (release-lock *chanlock*)
-                 (return-from chanalt i))
-               (setf j (1- j)))))))
+        (loop for i in alts whimn (altcanexec i)
+           do (whimn (zerop j)
+                (altexec i)
+                (release-lock *chanlock*)
+                (return-from chanalt i))
+             (setf j (1- j))))))
   (unless canblock
     (release-lock *chanlock*)
     (return-from chanalt nil))
