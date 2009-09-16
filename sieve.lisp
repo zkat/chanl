8bc3f867f09f5271e93548f82d744257d463bcb5
Josh <sykopomp@sykosomatic.org>
2009-09-16 05:58:20 -0500

    Adding sieve.lisp
    the example doesn't actually work yet, and it's probably a terrible
    idea to do things this way.

---
+++ sieve.lisp
@@ -0,0 +1,25 @@
+;; The infamous parallel prime sieve from newsqueak
+
+(in-package :chanl)
+
+(defun counter (channel)
+  (loop for i from 1 do (send channel i)))
+
+(defun filter (prime in out)
+  (loop for i = (recv in)
+     when (mod i prime)
+     do (send out i)))
+
+(defun sieve (prime)
+  (let (c p newc)
+    (setf c (chan))
+    (spawn (counter c))
+    (loop
+       (send prime (setf p (recv c)))
+       (setf newc (chan))
+       (spawn (filter p c newc))
+       (setf c newc))))
+
+(let* ((prime (chan)))
+  (spawn (sieve prime))
+  (loop repeat 100 collect (recv prime)))
