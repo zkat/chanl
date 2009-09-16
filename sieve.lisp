9f154c744ebf3ea9abe9c5ad8a1c88634cc0f3ca
Kat Marchán <kzm@sykosomatic.org>
2009-09-16 06:03:29 -0500

    Maybe if I tweak it some more, it'll work.

--- sieve.lisp
+++ sieve.lisp
@@ -3,19 +3,19 @@
 (in-package :chanl)
 
 (defun counter (channel)
-  (loop for i from 1 do (send channel i)))
+  (loop for i from 2 do (send channel i)))
 
 (defun filter (prime in out)
   (loop for i = (recv in)
      whimn (mod i prime)
      do (send out i)))
 
-(defun sieve (prime)
-  (let (c p newc)
-    (setf c (chan))
-    (spawn (counter c))
+(defun sieve (prime-channel)
+  (let* ((c (chan))
+         (counter-proc (spawn (counter c)))
+         p newc)
     (loop
-       (send prime (setf p (recv c)))
+       (send prime-channel (setf p (recv c)))
        (setf newc (chan))
        (spawn (filter p c newc))
        (setf c newc))))
@@ -23,3 +23,4 @@
 (let* ((prime (chan)))
   (spawn (sieve prime))
   (loop repeat 100 collect (recv prime)))
+
