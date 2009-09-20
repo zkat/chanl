617702c5a10736151c4a9830e8c254bd78dc9040
Kat Marchán <kzm@sykosomatic.org>
2009-09-19 23:32:19 -0500

    Hooray for print-debugging!

--- sieve.lisp
+++ sieve.lisp
@@ -3,22 +3,30 @@
 (in-package :chanl)
 
 (defun counter (channel)
-  (loop for i from 2 do (send channel i)))
+  (loop for i from 2 do (send channel i) (format t "COUNTER: Sent out ~A~%" i)))
 
 (defun filter (prime in out)
+  (format t "FILTER: filtering ~A using numbers from ~A, into ~A~%" prime in out)
   (loop for i = (recv in)
+     do (format t "FILTER: Received ~A~%" i)
      whimn (plusp (mod i prime))
-     do (send out i)))
+     do (format t "FILTER: Attempting to send out ~A~%" i)
+       (send out i)
+       (format t "FILTER: Sent out ~A~%" i)))
 
 (defun sieve ()
   (let* ((c (chan))
          (prime-chan (chan)))
     (spawn (counter c))
     (spawn (loop
+              (format t "SIEVE: Trying to read from c...~%")
               (let* ((prime (recv c))
                      (newc (chan)))
+                (format t "SIEVE: Received ~A from c~%" prime)
                 (send prime-chan prime)
+                (format t "SIEVE: Sent ~A down prime-chan~%" prime)
                 (spawn (filter prime c newc))
+                (format t "SIEVE: Spawned new FILTER process, going from ~A to ~A~%" c newc)
                 (setf c newc))))
     prime-chan))
 
