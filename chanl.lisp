8250ab9466b1a39b97b236d624ee87dee864ffe5
Josh <sykopomp@sykosomatic.org>
2009-09-19 23:31:57 -0500

    Added print-object method again

--- chanl.lisp
+++ chanl.lisp
@@ -97,6 +97,8 @@ new thread's name."
    of the channel's buffer (default 0)"
   (make-channel))
 
+(defmethod print-object ((channel channel) (s stream))
+  (print-unreadable-object (channel s :type t :identity t)))
 ;;;
 ;;; muxing macro
 ;;;
