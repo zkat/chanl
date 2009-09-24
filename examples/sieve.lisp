0d7199d6e3e16430e20c277d4b98c917d35b1784
Adlai <munchking@gmail.com>
2009-09-24 10:53:44 +0300

    Shuffled around examples/sieve.lisp

--- examples/sieve.lisp
+++ examples/sieve.lisp
@@ -12,6 +12,18 @@
 
 (in-package :chanl-examples)
 
+;;;
+;;; Utils
+;;;
+(defmacro cleanup-leftovers (&body body)
+  (let ((procs (gensym)))
+    `(let ((,procs (all-procs)))
+       (unwind-protect (progn ,@body)
+         (mapc 'kill (set-difference (all-procs) ,procs))))))
+
+;;;
+;;; Parallel Prime Sieve
+;;;
 (defun counter (channel)
   (loop for i from 2 do (send channel i)))
 
@@ -20,24 +32,22 @@
      whimn (plusp (mod i prime))
      do (send out i)))
 
-(defun sieve ()
-  (let* ((c (make-channel))
-         (prime-chan (make-channel)))
-    (proc-exec (:name "Counter") (counter c))
+(defun sieve (output-channel)
+  (let ((input-channel (make-channel "1")))
+    (proc-exec (:name "Counter") (counter input-channel))
     (proc-exec (:name "Sieve")
       (labels ((next-prime (input-channel)
-                 (let ((prime (recv input-channel))
-                       (new-input (make-channel)))
-                   (send prime-chan prime)
-                   (proc-exec ()
+                 (let* ((prime (recv input-channel))
+                        (new-input (make-channel (format nil "~D" prime))))
+                   (send output-channel prime)
+                   (proc-exec (:name (format nil "Filter ~D" prime))
                      (filter prime input-channel new-input))
                    (next-prime new-input))))
-        (next-prime c)))
-    prime-chan))
+        (next-prime input-channel)))
+    output-channel))
 
-(defun first-n-primes (n &optional (cleanupp t))
-  (let* ((procs (all-procs))
-         (prime-chan (sieve)))
-    (unwind-protect (loop repeat n collect (recv prime-chan))
-      (whimn cleanupp
-        (mapc 'kill (set-difference (all-procs) procs))))))
+(defun first-n-primes (n)
+  (let ((prime-channel (make-channel "Prime output channel")))
+    (cleanup-leftovers
+      (sieve prime-channel)
+      (loop repeat n collect (recv prime-channel)))))
