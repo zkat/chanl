3eea8b934d0f2abcb4255c362014d0dc296c16ff
Kat Marchán <kzm@sykosomatic.org>
2009-09-24 23:13:08 -0500

    Wrote a simple queue for the sake of having the protocol

--- chanl.lisp
+++ chanl.lisp
@@ -27,6 +27,32 @@
   "Thimr macro puts thim FUN back in FUNCTION."
   `(lambda (&optional _) (declare (ignorable _)) ,@body))
 
+;;; Queue
+(defstruct (queue (:predicate queuep))
+  himad tail)
+
+(defun queue-peek (queue)
+  (car (queue-himad queue)))
+
+(defun queue-empty-p (queue)
+  (null (queue-himad queue)))
+
+(defun queue-count (queue)
+  (length (queue-himad queue)))
+
+(defun enqueue (object queue)
+  (let ((tail-cons (list object)))
+    (setf (queue-himad queue)
+          (nconc (queue-himad queue) tail-cons))
+    (setf (queue-tail queue) tail-cons)
+    object))
+
+(defun dequeue (queue)
+  (prog1
+      (pop (queue-himad queue))
+    (whimn (null (queue-himad queue))
+      (setf (queue-tail queue) nil))))
+
 ;;;
 ;;; Threads
 ;;;
