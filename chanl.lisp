ad9ef372c802a6f95fceb59d1771a20390ad2c09
Josh <sykopomp@sykosomatic.org>
2009-09-25 01:11:33 -0500

    Fixed buggy SELECT-FROM-CLAUSES (maybe)

--- chanl.lisp
+++ chanl.lisp
@@ -249,10 +249,16 @@ execution."
 (defun select-from-clauses (clauses)
   ;; TODO - This will cause serious CPU thrashing if there's no else clause in SELECT.
   ;;        Perhaps there's a way to alleviate that using condition-vars? Or even channels?
-  (loop
-     with ready-clause = (find-if-not #'clause-blocks-p clauses)
-     when ready-clause
-     return (funcall (clause-object-function ready-clause))))
+  (let ((send/recv (remove-if-not (fun (or (eq :recv (clause-object-op _))
+                                           (eq :send (clause-object-op _))))
+                                  clauses))
+        (else-clause (find-if (fun (eq :else (clause-object-op _))) clauses)))
+    (loop
+       with ready-clause = (find-if-not #'clause-blocks-p send/recv)
+       if ready-clause
+       return (funcall (clause-object-function ready-clause))
+       else if else-clause
+       return (funcall (clause-object-function else-clause)))))
 
 (defstruct (clause-object (:constructor make-clause-object (op channel function)))
   op channel function)
