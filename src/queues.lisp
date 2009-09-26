1efe3738feca09f2ec5a676229bf67faa2451349
Adlai <munchking@gmail.com>
2009-09-27 01:46:20 +0200

    Fixed some queue bugs and optimized

--- src/queues.lisp
+++ src/queues.lisp
@@ -8,68 +8,64 @@
 
 (in-package :chanl)
 
-(setf *print-circle* t)                 ; Horrible hack, just in case... - Adlai
+(eval-when (:compile-toplevel)
+  (defvar queue-sentinel (make-symbol "EMPTY")))
 
-(defun make-queue (length)
+(define-speedy-function make-queue (length)
+    ((integer 1 #.(- array-total-size-limit 2)) length)
   "Creates a new queue of maximum size LENGTH"
-  (declare ;(optimize (speed 3) (safety 0) (debug 0))
-           (type (integer 1 #.(- array-total-size-limit 2)) length))
   (let ((queue (the simple-vector
                  (make-array (the (integer 3 #.array-total-size-limit)
-                               (+ 2 length))))))
-    (setf (svref queue 0) 2
-          (svref queue 1) 2
-          (svref queue 2) queue)))
+                               (+ 2 length))
+                             :initial-element 2))))
+    (setf (svref queue 2) '#.queue-sentinel)
+    queue))
 
-(defun queue-peek (queue)
+(define-speedy-function queue-peek (queue)
+    (simple-vector queue)
   "Dereference QUEUE's head pointer"
-  (declare ;(optimize (speed 3) (safety 0) (debug 0))
-           (type simple-vector queue))
   (svref queue (the (integer 2 #.(1- array-total-size-limit))
                  (svref queue 0))))
 
-(defun queue-zero-p (queue)
+(define-speedy-function queue-zero-p (queue)
+    (simple-vector queue)
   "Checks whether QUEUE's theoretical length is zero"
-  (declare ;(optimize (speed 3) (safety 0) (debug 0))
-           (type simple-vector queue))
   (= (the (integer 2 #.(1- array-total-size-limit))
        (svref queue 0))
      (the (integer 2 #.(1- array-total-size-limit))
        (svref queue 1))))
 
-(defun queue-empty-p (queue)
+(define-speedy-function queue-empty-p (queue)
+    (type simple-vector queue)
   "Checks whether QUEUE's effective length is zero"
-  (declare ;(optimize (speed 3) (safety 0) (debug 0))
-           (type simple-vector queue))
   (and (queue-zero-p queue)
-       (eq (queue-peek queue) queue)))
+       (eq (queue-peek queue) '#.queue-sentinel)))
 
-(defun queue-full-p (queue)
+(define-speedy-function queue-full-p (queue)
+    (type simple-vector queue)
   "Checks whether QUEUE is effectively full"
-  (declare ;(optimize (speed 3) (safety 0) (debug 0))
-           (type simple-vector queue))
   (and (queue-zero-p queue)
-       (not (eq (queue-peek queue) queue))))
+       (not (eq (queue-peek queue) '#.queue-sentinel))))
 
-(defun queue-count (queue)
+(define-speedy-function queue-count (queue)
+    (simple-vector queue)
   "Returns QUEUE's effective length"
-  (declare ;(optimize (speed 3) (safety 0) (debug 0))
-           (type simple-vector queue))
   (let ((length (mod (- (svref queue 1)
                         (svref queue 0))
                      (- (length queue) 2))))
     (if (zerop length)
-        (if (eq (queue-peek queue) queue) 0
+        (if (eq (queue-peek queue) '#.queue-sentinel) 0
             (- (length queue) 2))
         length)))
 
-(defun queue-max-size (queue)
-  (- (length queue) 2))
+(define-speedy-function queue-max-size (queue)
+    (simple-vector queue)
+  "Returns QUEUE's maximum length"
+  (the fixnum (- (length queue) 2)))
 
-(defun enqueue (object queue)
+(define-speedy-function enqueue (object queue)
+    (simple-vector queue)
   "Sets QUEUE's head to OBJECT and increments QUEUE's head pointer"
-  (declare ;(optimize (speed 3) (safety 0) (debug 0))
-           (type simple-vector queue))
   (setf (svref queue (the (integer 2 #.(1- array-total-size-limit))
                        (svref queue 1)))
         object
@@ -78,10 +74,9 @@
                   (- (length queue) 2))))
   object)
 
-(defun dequeue (queue)
+(define-speedy-function dequeue (queue)
+    (simple-vector queue)
   "Sets QUEUE's tail to QUEUE, increments QUEUE's tail pointer, and returns the previous tail ref"
-  (declare ;(optimize (speed 3) (safety 0) (debug 0))
-          (type simple-vector queue))
   (prog1 (svref queue (svref queue 0))
-    (setf (svref queue (svref queue 0)) queue
-          (svref queue 0) (+ 2 (mod (1- (svref queue 0)) (- (length queue) 2))))))
+    (setf (svref queue 0) (+ 2 (mod (1- (svref queue 0)) (- (length queue) 2))))
+    (when (queue-zero-p queue) (setf (svref queue (svref queue 0)) '#.queue-sentinel))))
