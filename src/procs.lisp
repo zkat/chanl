65ddc4d40fd56ff3f4419727c6839baa7f87e9ac
Adlai <munchking@gmail.com>
2009-09-26 10:18:36 +0300

    Split off src/procs.lisp

---
+++ src/procs.lisp
@@ -0,0 +1,40 @@
+;;;; -*- Mode: Lisp; Syntax: ANSI-Common-Lisp; Base: 10; indent-tabs-mode: nil -*-
+;;;;
+;;;; Copyright © 2009 Josh Marchan
+;;;;
+;;;; Process Abstraction
+;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(in-package :chanl)
+
+(defun current-proc ()
+  (bt:current-thread))
+
+(defun proc-alive-p (proc)
+  (bt:thread-alive-p proc))
+
+(defun procp (proc)
+  (bt:threadp proc))
+
+(defun proc-name (proc)
+  (bt:thread-name proc))
+
+(defun kill (proc)
+  (bt:destroy-thread proc))
+
+(defun pcall (function &key name (initial-bindings *default-special-bindings*))
+  "PCALL -> Parallel Call; calls FUNCTION in a new thread. FUNCTION must be a no-argument
+function. Providing NAME will set the thread's name. Refer to Bordeaux-threads documentation
+for how INITIAL-BINDINGS works."
+  (bt:make-thread function :name name :initial-bindings initial-bindings))
+
+(defmacro pexec ((&key name initial-bindings) &body body)
+  "Executes BODY in parallel (a new thread). NAME sets new thread's name. Refer to
+Bordeaux-Threads documentation for more information on INITIAL-BINDINGS."
+  `(pcall (lambda () ,@body)
+          ,@(when name `(:name ,name))
+          ,@(when initial-bindings `(:initial-bindings ,initial-bindings))))
+
+(defun all-procs ()
+  (bt:all-threads))
