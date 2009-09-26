0fa8f179cd4a58e7af6198db21af0aaeb813cba3
Adlai <munchking@gmail.com>
2009-09-26 09:53:38 +0300

    Split off src/packages.lisp

--- src/channels.lisp
+++ src/channels.lisp
@@ -4,20 +4,6 @@
 ;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(defpackage :chanl
-  (:use :common-lisp)
-  (:import-from :bordeaux-threads :*default-special-bindings*)
-  (:export
-   ;; processes
-   #:pcall #:pexec #:kill #:current-proc
-   #:proc-alive-p #:procp #:proc-name
-   #:*default-special-bindings* #:all-procs
-   ;; channels
-   #:make-channel #:send #:recv
-   #:channel #:send-blocks-p #:recv-blocks-p
-   ;; selecting!
-   #:select #:send-select #:recv-select))
-
 (in-package :chanl)
 
 ;;;
