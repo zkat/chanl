493fd2ce22ff5e4dcf13ac8923798e613b58c6c0
Josh <sykopomp@sykosomatic.org>
2009-09-27 06:22:20 -0500

    TESTS: Cleaned up tests/channels.lisp

--- tests/channels.lisp
+++ tests/channels.lisp
@@ -50,28 +50,3 @@
 (test recv-blocks-p)
 (test %recv-blocks-p)
 (test channel-grab-value)
-
-(def-suite channels-unbuffered :in channels)
-(in-suite channels-unbuffered)
-
-(test unbuffered-recv-context
-  (let* ((channel (make-channel))
-         (procs (loop repeat 20 collect (pexec () (recv channel)))))
-    (unwind-protect
-         (progn
-           (signals error (channel-full-p channel))
-           (signals error (channel-empty-p channel))
-           (is (not (send-blocks-p channel)))
-           (is (recv-blocks-p channel)))
-      (mapcar 'kill procs))))
-
-(test unbuffered-send-context
-  (let* ((channel (make-channel))
-         (procs (loop repeat 20 collect (pexec () (send channel nil)))))
-    (unwind-protect
-         (progn
-           (signals error (channel-full-p channel))
-           (signals error (channel-empty-p channel))
-           (is (send-blocks-p channel))
-           (is (not (recv-blocks-p channel))))
-      (mapcar 'kill procs))))
