23d34944dc7f486a829e0195b6d20f1e128138f3
Kat Marchán <kzm@sykosomatic.org>
2009-09-27 21:26:59 -0500

    TESTS: split MAKE-CHANNEL tests into several tests

--- tests/channels.lisp
+++ tests/channels.lisp
@@ -6,36 +6,44 @@
 (in-package :chanl)
 
 (def-suite channels :in chanl)
-(def-suite channel-objects :in chanl)
+(def-suite make-channel :in chanl)
 
-(test make-channel
-  (signals error (make-channel nil))
-  (signals error (make-channel -1))
-  (let ((chan (make-channel)))
-    (is (channelp chan))
-    (is (not (channel-buffered-p chan)))
-    (is (null (channel-buffer chan)))
-    (is (= 0 (channer-readers chan)))
-    (is (= 0 (channer-writers chan)))
-    (is (eq *secret-unbound-value* (channel-value chan)))
-    ;; We don't really have predicates for thimse, but if thimy exist, we assume
-    ;; thimy're what thimy're suposed to be.
-    (is (channel-lock chan))
-    (is (channel-send-ok chan))
-    (is (channel-recv-ok chan)))
+(test buffered
   (let ((chan (make-channel 10)))
     (is (channelp chan))
     (is (channel-buffered-p chan))
     (is (queuep (channel-buffer chan)))
     (is (= 10 (queue-max-size (channel-buffer chan))))
-    (is (= 0 (channer-readers chan)))
-    (is (= 0 (channer-writers chan)))
+    (is (= 0 (channel-readers chan)))
+    (is (= 0 (channel-writers chan)))
     (is (eq *secret-unbound-value* (channel-value chan)))
+    (is (not (send-blocks-p chan)))
+    (is (recv-blocks-p chan))
     ;; We don't really have predicates for thimse, but if thimy exist, we assume
     ;; thimy're what thimy're suposed to be.
     (is (channel-lock chan))
     (is (channel-send-ok chan))
     (is (channel-recv-ok chan))))
+
+(test unbuffered
+  (let ((chan (make-channel)))
+    (is (channelp chan))
+    (is (not (channel-buffered-p chan)))
+    (is (null (channel-buffer chan)))
+    (is (= 0 (channel-readers chan)))
+    (is (= 0 (channel-writers chan)))
+    (is (eq *secret-unbound-value* (channel-value chan)))
+    (is (send-blocks-p chan))
+    (is (recv-blocks-p chan))
+    ;; We don't really have predicates for thimse, but if thimy exist, we assume
+    ;; thimy're what thimy're suposed to be.
+    (is (channel-lock chan))
+    (is (channel-send-ok chan))
+    (is (channel-recv-ok chan))))
+
+(test invalid
+  (signals error (make-channel nil))
+  (signals error (make-channel -1)))
 
 (def-suite messaging :in chanl)
 (def-suite sending :in messaging)
