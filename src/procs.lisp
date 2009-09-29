052e804b04116390e5fad9071ed4b39530032eb7
Kat Marchán <kzm@sykosomatic.org>
2009-09-28 21:03:19 -0500

    THREAD-POOL: Initial adaptation of code.

--- src/procs.lisp
+++ src/procs.lisp
@@ -9,50 +9,50 @@
 (in-package :chanl)
 
 (defclass thread-pool ()
-  ((threads :accessor pool-threads :initform ())
+  ((threads :accessor pool-threads :initform nil)
    (free-thread-counter :accessor free-thread-counter :initform 0)
-   (soft-limit :accessor pool-soft-limit :initform nil)
-   (lock :reader pool-lock :initform (make-lock "thread pool lock"))
-   (leader-lock :reader pool-leader-lock :initform (make-lock "thread leader lock"))
-   (leader-notifier :reader pool-leader-notifier :initform (make-condition-variable))
+   (soft-limit :accessor pool-soft-limit :initform 1000) ; this seems like a sane-ish default
+   (lock :reader pool-lock :initform (bt:make-lock "thread pool lock"))
+   (leader-lock :reader pool-leader-lock :initform (bt:make-lock "thread leader lock"))
+   (leader-notifier :reader pool-leader-notifier :initform (bt:make-condition-variable))
    (tasks :accessor pool-tasks :initform nil)))
 
 (defvar *thread-pool* (make-instance 'thread-pool))
 
-(define-symbol-macro %thread-pool-soft-limit (soft-limit *thread-pool*))
+(define-symbol-macro %thread-pool-soft-limit (pool-soft-limit *thread-pool*))
 
-(defun new-worker-thread (thread-pool task)
+(defun new-worker-thread (thread-pool &optional task)
   (push (bt:make-thread
          (lambda ()
            (unwind-protect
-                (loop (when task (ignore-errors (funcall task)))
-                   (with-lock-held ((lock thread-pool))
-                     (if (and (soft-limit thread-pool)
-                              (> (length (threads thread-pool))
-                                 (soft-limit thread-pool)))
+                (loop (when task (funcall task))
+                   (bt:with-lock-held ((lock thread-pool))
+                     (if (and (pool-soft-limit thread-pool)
+                              (> (length (pool-threads thread-pool))
+                                 (pool-soft-limit thread-pool)))
                          (return)
                          (incf (free-thread-counter thread-pool))))
-                   (with-lock-held ((leader-lock thread-pool))
-                     (with-lock-held ((lock thread-pool))
+                   (bt:with-lock-held ((pool-leader-lock thread-pool))
+                     (bt:with-lock-held ((pool-lock thread-pool))
                        (setf task
-                             (or #1=(pop (tasks thread-pool))
-                                 (progn
-                                   (condition-wait (leader-notifier thread-pool)
-                                                   (lock thread-pool))
-                                   #1#)))
+                             (loop until (pool-tasks thread-pool)
+                                do (bt:condition-wait (pool-leader-notifier thread-pool)
+                                                      (pool-lock thread-pool))
+                                finally (return (pop (pool-tasks thread-pool)))))
                        (decf (free-thread-counter thread-pool)))))
-             (with-lock-held ((lock thread-pool))
-               (setf (threads thread-pool)
-                     (remove (current-thread) (threads thread-pool))))))
-         :name "Eager Futures Thread Pool Worker")
-        (threads thread-pool)))
+             (bt:with-lock-held ((pool-lock thread-pool))
+               (setf (pool-threads thread-pool)
+                     (delete (current-thread) (pool-threads thread-pool))))))
+         :name "ChanL Thread Pool Worker")
+        (pool-threads thread-pool)))
 
 (defmethod assign-task (task (thread-pool thread-pool))
-  (with-lock-held ((lock thread-pool))
-    (if (= (free-thread-counter thread-pool) (length (tasks thread-pool)))
+  (bt:with-lock-held ((pool-lock thread-pool))
+    (if (= (free-thread-counter thread-pool) (length (pool-tasks thread-pool)))
         (new-worker-thread thread-pool task)
-        (push task (tasks thread-pool))))
-  (condition-notify (leader-notifier thread-pool)))
+        (setf (pool-tasks thread-pool)
+              (nconc (pool-tasks thread-pool) (list task)))))
+  (bt:condition-notify (pool-leader-notifier thread-pool)))
 
 (defun current-proc ()
   (bt:current-thread))
@@ -69,18 +69,15 @@
 (defun kill (proc)
   (bt:destroy-thread proc))
 
-(defun pcall (function &key name (initial-bindings *default-special-bindings*))
-  "PCALL -> Parallel Call; calls FUNCTION in a new thread. FUNCTION must be a no-argument
-function. Providing NAME will set the thread's name. Refer to Bordeaux-threads documentation
-for how INITIAL-BINDINGS works."
-  (bt:make-thread function :name name :initial-bindings initial-bindings))
+(defun pcall (function)
+  "PCALL -> Parallel Call; calls FUNCTION in a new thread. FUNCTION must be a no-argument function."
+  (assign-task function)
+  t)
 
-(defmacro pexec ((&key name initial-bindings) &body body)
-  "Executes BODY in parallel (a new thread). NAME sets new thread's name. Refer to
-Bordeaux-Threads documentation for more information on INITIAL-BINDINGS."
-  `(pcall (lambda () ,@body)
-          ,@(when name `(:name ,name))
-          ,@(when initial-bindings `(:initial-bindings ,initial-bindings))))
+(defmacro pexec (() &body body)
+  ;; note: the () is present because there -will- be options.
+  "Executes BODY in parallel."
+  `(pcall (lambda () ,@body)))
 
 (defun all-procs ()
   (bt:all-threads))
