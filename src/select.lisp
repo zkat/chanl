e6f7c7f2ab3ca6c5670b760d32e1e2899e5d2fa5
Adlai <munchking@gmail.com>
2009-09-29 12:57:21 +0200

    SELECT: Flushing out old code

--- src/select.lisp
+++ src/select.lisp
@@ -10,7 +10,7 @@
 ;;;
 ;;; Select macro
 ;;;
-(defmacro select (&body body)
+(defmacro select (&body clauses)
   "Non-deterministically select a non-blocking clause to execute.
 
 The syntax is:
@@ -33,67 +33,5 @@ SELECT will block until one of the clauses is available for execution.
 SELECT's non-determinism is, in fact, very non-deterministic. Clauses are chosen at random, not
 in the order they are written. It's worth noting that SEND/RECV, when used on sequences of
 channels, are still linear in the way they go through the sequence -- the random selection is
-reserved for individual SELECT clauses."
-  `(select-from-clauses
-    (list ,@(loop for clause in body
-               collect (clause->make-clause-object clause)))))
 
-(defun determine-op (clause)
-  (cond ((and (not (listp (car clause)))
-              (or (eq t (car clause))
-                  (equal "ELSE" (symbol-name (car clause)))
-                  (equal "OTHERWISE" (symbol-name (car clause)))))
-         :else)
-        ((listp (car clause))
-         (let ((clause-name (symbol-name (caar clause))))
-           (cond ((string= clause-name "SEND") :send)
-                 ((string= clause-name "RECV") :recv)
-                 (t (error "Invalid clause type ~A" (car clause))))))
-        (t (error "Invalid clause type ~A" (car clause)))))
 
-(defstruct (clause (:constructor make-clause (op try-fun body-fun)))
-  op try-fun body-fun)
-
-(defun clause->make-clause-object (clause)
-  (let ((op (determine-op clause)))
-    (multiple-value-bind (try-fun body-fun)
-        (parse-clause op clause)
-      `(make-clause-object ,op ,try-fun ,body-fun))))
-
-(defun select-from-clauses (clauses)
-  ;; todo
-  )
-
-(defun try-clause (clause)
-  ;; NO IDEA
-  (case (clause-op clause)
-    (:else
-     (clause-body-fun clause))
-    (:send
-     (let ((chan (funcall (clause-try-fun clause))))
-       (when chan
-         (funcall (clause-body-fun )))))))
-
-(defun parse-clause (op clause)
-  (let (channel attempt-fun body)
-    ;; what should these even look like? How do we make send/recv's return values available to
-    ;; the clause body even though we don't want to run the body unless we get a sensible value?...
-    (case op
-      (:else
-       (setf body (cdr clause)))
-      (:send
-       (destructuring-bind (chan value &optional channel-var)
-           (cdr clause)
-         (setf attempt-fun `(lambda () (send ,chan ,value nil)))
-         (setf body (if channel-var
-                        `(let ((,channel-var (funcall ,attempt-fun))))))
-         )
-       (setf channel (cadar clause))
-       (setf body clause))
-      (:recv
-       (setf channel (cadar clause))
-       (setf body (if (= 3 (length (car clause)))
-                      `((let ((,(third (car clause)) ,(butlast (car clause))))
-                          ,@(cdr clause)))
-                      clause))))
-    (values attempt-fun `(lambda () ,@body))))
