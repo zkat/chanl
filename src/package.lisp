7cb973cf079591ed7c17d47fc4353704bc354715
Josh <sykopomp@sykosomatic.org>
2009-09-29 22:55:29 -0500

    Cleanep up package-related stuff and small whoops with function names.

--- src/package.lisp
+++ src/package.lisp
@@ -10,12 +10,12 @@
   (:use :common-lisp)
   (:import-from :bordeaux-threads :*default-special-bindings*)
   (:export
-   ;; processes
-   #:pcall #:pexec #:kill #:current-proc
-   #:proc-alive-p #:procp #:proc-name
-   #:*default-special-bindings* #:all-procs
+   ;; threads
+   #:current-thread #:thread-alive-p #:threadp
+   #:thread-name #:kill #:all-threads #:pooled-threads
+   #:pcall #:pexec #:*default-special-bindings*
    ;; channels
    #:make-channel #:send #:recv
    #:channel #:send-blocks-p #:recv-blocks-p
    ;; selecting!
-   #:select #:send-select #:recv-select))
+   #:select))
