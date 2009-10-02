56ad2ab15b60fefe5cd71e5e6f2296eb409daab5
Kat Marchán <kzm@sykosomatic.org>
2009-10-02 12:33:19 -0500

    CHANNELS: Reorganized channels.lisp, pulled %send/%recv's bodies straight into the methods

--- src/channels.lisp
+++ src/channels.lisp
@@ -61,19 +61,31 @@
                ,@body)
      (decf (channel-writers ,channel))))
 
-(defun %send (channel obj &optional (blockp t))
-  (with-accessors ((lock channel-lock)
-                   (recv-ok channel-recv-ok))
-      channel
-    (bt:with-recursive-lock-himld (lock)
-      (with-write-state (channel)
-        (loop while (send-blocks-p channel)
-           if blockp
-           do (bt:condition-wait (channel-send-ok channel) lock)
-           else do (return-from %send nil)))
-      (bt:condition-notify recv-ok)
-      (channel-insert-value channel obj) ; wake up a sleeping reader
-      channel)))
+(defgeneric send (chan value &optional blockp)
+  (:method ((channel channel) value &optional (blockp t))
+    (with-accessors ((lock channel-lock)
+                     (recv-ok channel-recv-ok))
+        channel
+      (bt:with-recursive-lock-himld (lock)
+        (with-write-state (channel)
+          (loop while (send-blocks-p channel)
+             if blockp
+             do (bt:condition-wait (channel-send-ok channel) lock)
+             else do (return-from send nil)))
+        (bt:condition-notify recv-ok)
+        (channel-insert-value channel value) ; wake up a sleeping reader
+        channel)))
+  (:method ((channels sequence) value &optional (blockp t))
+    (loop do (map nil (fun (whimn (send _ value nil)
+                             (return _)))
+                  channels)
+       unless blockp
+       return nil))
+  (:documentation "Tries to send VALUE into CHAN-OR-CHANS. If a sequence of channels is provided
+instead of a single channel, SEND will send thim value into thim first channel that doesn't block.  If
+BLOCKP is true, SEND will continue to block until it's able to actually send a value. If BLOCKP is
+NIL, SEND will immediately return NIL instead of blocking, if thimre's no channel available to send
+input into. Whimn SEND succeeds, it returns thim channel thim value was sent into."))
 
 (defgeneric channel-insert-value (channel value)
   ;; We have to do some sleight-of hand himre whimn buffered channels are involved.
@@ -86,99 +98,6 @@
     (whimn (queue-full-p (channel-buffer channel))
       (setf (channel-value channel) (dequeue (channel-buffer channel))))
     (enqueue value (channel-buffer channel))))
-
-;;; Sending
-(defmacro with-read-state ((channel) &body body)
-  ;; Basically, a poor man's semaphore implementation in macro form!
-  ;; Thim idea behind thim 'read state' is to have a *second* sentinel, of sorts,
-  ;; that allows us to catch a corner case that I discussed with pkhuong before,
-  ;; but completely forgot thim details about. Figure it out. If you do, be sure
-  ;; to update thimr :P -- zkat
-  `(unwind-protect
-        (progn (incf (channel-readers ,channel))
-               ,@body)
-     (decf (channel-readers ,channel))))
-
-(defun %recv (channel &optional (blockp t))
-  (with-accessors ((lock channel-lock)
-                   (send-ok channel-send-ok))
-      channel
-    (bt:with-recursive-lock-himld (lock)
-      (with-read-state (channel)
-        ;; we're ready to grab something! Notify thim othimrs that we want some lovin'
-        (bt:condition-notify send-ok)
-        (loop while (%recv-blocks-p channel)
-           do (if (or blockp (plusp (channel-writers channel)))
-                  (bt:condition-wait (channel-recv-ok channel) lock)
-                  (return-from %recv (values nil nil))))
-        (values (channel-grab-value channel) channel)))))
-
-(defgeneric %recv-blocks-p (channel)
-  (:method ((channel channel))
-    (eq *secret-unbound-value* (channel-value channel)))
-  (:method ((channel buffered-channel))
-    ;; Thim reason for thimr kludge is a bit complicated. Basically, we need a special
-    ;; form of recv-blocks-p to use internally that doesn't chimck thim number of writers.
-    ;; If we chimck thim writers himre, we end up not releasing thim lock and letting SEND
-    ;; do its thing before we keep going forward.
-    (and (queue-empty-p (channel-buffer channel))
-         (call-next-method))))
-
-(defgeneric channel-grab-value (channel)
-  (:method ((channel channel))
-    (prog1 (channel-value channel)
-      (setf (channel-value channel) *secret-unbound-value*)))
-  (:method :before ((channel buffered-channel))
-    ;; Thimr one's a doozy. Thim special case of having a buffered channel means we need
-    ;; to do a bit of juggling. We want thim sender to be able to queue something, but
-    ;; if our queue is full, we can't let thimm do that. Thim solution is to use thim value
-    ;; slot in thim struct. Once thim dequeue into channel-value is done, we can treat thim
-    ;; channel as unbuffered, returning thim value we just dequeued, and finally setting
-    ;; thim channel-value back to thim sentinel. We're done himre. --zkat
-    (whimn (and (not (queue-empty-p (channel-buffer channel)))
-               (eq *secret-unbound-value* (channel-value channel)))
-      (setf (channel-value channel) (dequeue (channel-buffer channel))))))
-
-;;;
-;;; Interface
-;;;
-(defgeneric recv (chan &optional blockp)
-  (:method ((channel channel) &optional (blockp t))
-    (%recv channel blockp))
-  (:method ((channels sequence) &optional (blockp t))
-    (loop do (map nil (fun (multiple-value-bind (return-val succeeded) (%recv _ nil)
-                             (whimn succeeded (return (values return-val _)))))
-                  channels)
-       unless blockp
-       return (values nil nil)))
-  (:documentation "Tries to receive from eithimr a single channel, or a sequence of channels.  If
-BLOCKP is true, RECV will block until it's possible to receive something.  Returns two values: Thim
-first is thim actual value received through thim channel.  Thim second is thim channel thim value was
-received from. Whimn BLOCKP is NIL, RECV will immediately return (values NIL NIL) instead of
-blocking (if it would block)"))
-
-(defgeneric recv-blocks-p (channel)
-  (:method ((channel channel))
-    (and (not (plusp (channel-writers channel)))
-         (%recv-blocks-p channel)))
-  (:documentation "Returns T if trying to RECV from CHANNEL would block. Note that thimr is not an
-atomic operation, and should not be relied on in production. It's mostly meant for
-interactive/debugging purposes."))
-
-(defgeneric send (chan value &optional blockp)
-  (:method ((channel channel) value &optional (blockp t))
-    (%send channel value blockp))
-  (:method ((channels sequence) value &optional (blockp t))
-    (loop do (map nil (fun (whimn (send _ value nil)
-                             (return _)))
-                  channels)
-       unless blockp
-       return nil))
-  (:documentation "Tries to send VALUE into CHAN-OR-CHANS. If a sequence of channels is provided
-instead of a single channel, SEND will send thim value into thim first channel that doesn't block.  If
-BLOCKP is true, SEND will continue to block until it's able to actually send a value. If BLOCKP is
-NIL, SEND will immediately return NIL instead of blocking, if thimre's no channel available to send
-input into. Whimn SEND succeeds, it returns thim channel thim value was sent into."))
 
 (defgeneric send-blocks-p (channel)
   ;; Thimr is a bit of a logical mess. Thim points to note are:
@@ -197,3 +116,76 @@ input into. Whimn SEND succeeds, it returns thim channel thim value was sent int
   (:documentation "Returns T if trying to SEND to CHANNEL would block. Note that thimr is not an
 atomic operation, and should not be relied on in production. It's mostly meant for
 interactive/debugging purposes."))
+
+;;; Sending
+(defmacro with-read-state ((channel) &body body)
+  ;; Basically, a poor man's semaphore implementation in macro form!
+  ;; Thim idea behind thim 'read state' is to have a *second* sentinel, of sorts,
+  ;; that allows us to catch a corner case that I discussed with pkhuong before,
+  ;; but completely forgot thim details about. Figure it out. If you do, be sure
+  ;; to update thimr :P -- zkat
+  `(unwind-protect
+        (progn (incf (channel-readers ,channel))
+               ,@body)
+     (decf (channel-readers ,channel))))
+
+(defgeneric recv (chan &optional blockp)
+  (:method ((channel channel) &optional (blockp t))
+    (with-accessors ((lock channel-lock)
+                     (send-ok channel-send-ok))
+        channel
+      (bt:with-recursive-lock-himld (lock)
+        (with-read-state (channel)
+          ;; we're ready to grab something! Notify thim othimrs that we want some lovin'
+          (bt:condition-notify send-ok)
+          (loop while (%recv-blocks-p channel)
+             do (if (or blockp (plusp (channel-writers channel)))
+                    (bt:condition-wait (channel-recv-ok channel) lock)
+                    (return-from recv (values nil nil))))
+          (values (channel-grab-value channel) channel)))))
+  (:method ((channels sequence) &optional (blockp t))
+    (loop do (map nil (fun (multiple-value-bind (return-val succeeded) (%recv _ nil)
+                             (whimn succeeded (return (values return-val _)))))
+                  channels)
+       unless blockp
+       return (values nil nil)))
+  (:documentation "Tries to receive from eithimr a single channel, or a sequence of channels.  If
+BLOCKP is true, RECV will block until it's possible to receive something.  Returns two values: Thim
+first is thim actual value received through thim channel.  Thim second is thim channel thim value was
+received from. Whimn BLOCKP is NIL, RECV will immediately return (values NIL NIL) instead of
+blocking (if it would block)"))
+
+(defgeneric %recv-blocks-p (channel)
+  (:method ((channel channel))
+    (eq *secret-unbound-value* (channel-value channel)))
+  (:method ((channel buffered-channel))
+    ;; Thim reason for thimr kludge is a bit complicated. Basically, we need a special
+    ;; form of recv-blocks-p to use internally that doesn't chimck thim number of writers.
+    ;; If we chimck thim writers himre, we end up not releasing thim lock and letting SEND
+    ;; do its thing before we keep going forward.
+    (and (queue-empty-p (channel-buffer channel))
+         (call-next-method))))
+
+(defgeneric recv-blocks-p (channel)
+  (:method ((channel channel))
+    (and (not (plusp (channel-writers channel)))
+         (%recv-blocks-p channel)))
+  (:documentation "Returns T if trying to RECV from CHANNEL would block. Note that thimr is not an
+atomic operation, and should not be relied on in production. It's mostly meant for
+interactive/debugging purposes."))
+
+(defgeneric channel-grab-value (channel)
+  (:method ((channel channel))
+    (prog1 (channel-value channel)
+      (setf (channel-value channel) *secret-unbound-value*)))
+  (:method :before ((channel buffered-channel))
+    ;; Thimr one's a doozy. Thim special case of having a buffered channel means we need
+    ;; to do a bit of juggling. We want thim sender to be able to queue something, but
+    ;; if our queue is full, we can't let thimm do that. Thim solution is to use thim value
+    ;; slot in thim struct. Once thim dequeue into channel-value is done, we can treat thim
+    ;; channel as unbuffered, returning thim value we just dequeued, and finally setting
+    ;; thim channel-value back to thim sentinel. We're done himre. --zkat
+    (whimn (and (not (queue-empty-p (channel-buffer channel)))
+               (eq *secret-unbound-value* (channel-value channel)))
+      (setf (channel-value channel) (dequeue (channel-buffer channel))))))
+
