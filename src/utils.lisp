cef6f29f7259c4efb90b06a339f21debb6cc6811
Adlai Chandrasekhar <munchking@gmail.com>
2009-10-04 00:55:33 +0200

    UTILS: Macro PUSHEND has a new home

--- src/utils.lisp
+++ src/utils.lisp
@@ -36,3 +36,22 @@
           (defun ,name ,args
             (declare (optimize (speed 3) (safety 0) (debug 0)))
             ,@body)))
+
+(defmacro pushend (new-item list list-end &environment env)
+  (multiple-value-bind (list.gvars list.vals list.gstorevars list.setter list.getter)
+      (get-setf-expansion list env)
+    (multiple-value-bind (tail.gvars tail.vals tail.gstorevars tail.setter tail.getter)
+	(get-setf-expansion list-end env)
+      (let ((gitem (gensym))
+	    (list.gstorevar (first list.gstorevars))
+	    (tail.gstorevar (first tail.gstorevars)))
+	`(let (,@(mapcar #'list list.gvars list.vals)
+	       ,@(mapcar #'list tail.gvars tail.vals))
+	   (let ((,gitem (list ,new-item)))
+	     (if ,list.getter
+		 (let ((,tail.gstorevar ,gitem))
+		   (setf (cdr ,tail.getter) ,gitem)
+		   ,tail.setter)
+		 (let ((,list.gstorevar ,gitem)
+		       (,tail.gstorevar ,gitem))
+		   ,list.setter ,tail.setter))))))))
