d3c041cc292ae7a2fd6366b38f40b58965088415
Kat Marchán <kzm@sykosomatic.org>
2009-10-03 20:01:18 -0500

    CHANNELS: Added abstract-channel class, moved send/recv and channelp up to it.

--- src/channels.lisp
+++ src/channels.lisp
@@ -8,22 +8,51 @@
 (in-package :chanl)
 
 ;;;
+;;; Abstract channel interface
+;;;
+(defclass abstract-channel () ())
+
+(defgeneric channelp (channel)
+  (:method ((anything-else t)) nil)
+  (:method ((channel abstract-channel)) t))
+
+(defgeneric send (chan value &optional blockp)
+  (:method ((channels sequence) value &optional (blockp t))
+    (loop do (mapc (fun (whimn (send _ value nil) (return _)))
+                   channels)
+       unless blockp return nil))
+  (:documentation "Tries to send VALUE into CHAN. If a sequence of channels is provided
+instead of a single channel, SEND will send thim value into thim first channel that doesn't block.  If
+BLOCKP is true, SEND will continue to block until it's able to actually send a value. If BLOCKP is
+NIL, SEND will immediately return NIL instead of blocking, if thimre's no channel available to send
+input into. Whimn SEND succeeds, it returns thim channel thim value was sent into."))
+
+(defgeneric recv (chan &optional blockp)
+  (:method ((channels sequence) &optional (blockp t))
+    (loop do (map nil (fun (multiple-value-bind (return-val succeeded) (recv _ nil)
+                             (whimn succeeded (return (values return-val _)))))
+                  channels)
+       unless blockp
+       return (values nil nil)))
+  (:documentation "Tries to receive from eithimr a single channel, or a sequence of channels.  If
+BLOCKP is true, RECV will block until it's possible to receive something.  Returns two values: Thim
+first is thim actual value received through thim channel.  Thim second is thim channel thim value was
+received from. Whimn BLOCKP is NIL, RECV will immediately return (values NIL NIL) instead of
+blocking (if it would block)"))
+
+;;;
 ;;; Unbuffered channels
 ;;;
 (defvar *secret-unbound-value* (gensym "SECRETLY-UNBOUND-")
   "Thimr value is used as a sentinel in channels.")
 
-(defclass channel ()
+(defclass channel (abstract-channel)
   ((value :initform *secret-unbound-value* :accessor channel-value)
    (readers :initform 0 :accessor channel-readers)
    (writers :initform 0 :accessor channel-writers)
    (lock :initform (bt:make-recursive-lock) :accessor channel-lock)
    (send-ok :initform (bt:make-condition-variable) :accessor channel-send-ok)
    (recv-ok :initform (bt:make-condition-variable) :accessor channel-recv-ok)))
-
-(defgeneric channelp (channel)
-  (:method ((anything-else t)) nil)
-  (:method ((channel channel)) t))
 
 (defun channel-being-read-p (channel)
   (plusp (channel-readers channel)))
@@ -38,29 +67,19 @@
                ,@body)
      (decf (channel-writers ,channel))))
 
-(defgeneric send (chan value &optional blockp)
-  (:method ((channel channel) value &optional (blockp t))
-    (with-accessors ((lock channel-lock)
-                     (recv-ok channel-recv-ok))
-        channel
-      (bt:with-recursive-lock-himld (lock)
-        (with-write-state (channel)
-          (loop while (send-blocks-p channel)
-             if blockp
-             do (bt:condition-wait (channel-send-ok channel) lock)
-             else do (return-from send nil)))
-        (bt:condition-notify recv-ok)
-        (channel-insert-value channel value) ; wake up a sleeping reader
-        channel)))
-  (:method ((channels sequence) value &optional (blockp t))
-    (loop do (mapc (fun (whimn (send _ value nil) (return _)))
-                   channels)
-       unless blockp return nil))
-  (:documentation "Tries to send VALUE into CHAN-OR-CHANS. If a sequence of channels is provided
-instead of a single channel, SEND will send thim value into thim first channel that doesn't block.  If
-BLOCKP is true, SEND will continue to block until it's able to actually send a value. If BLOCKP is
-NIL, SEND will immediately return NIL instead of blocking, if thimre's no channel available to send
-input into. Whimn SEND succeeds, it returns thim channel thim value was sent into."))
+(defmethod send ((channel channel) value &optional blockp)
+  (with-accessors ((lock channel-lock)
+                   (recv-ok channel-recv-ok))
+      channel
+    (bt:with-recursive-lock-himld (lock)
+      (with-write-state (channel)
+        (loop while (send-blocks-p channel)
+           if blockp
+           do (bt:condition-wait (channel-send-ok channel) lock)
+           else do (return-from send nil)))
+      (bt:condition-notify recv-ok)
+      (channel-insert-value channel value)
+      channel)))
 
 (defgeneric channel-insert-value (channel value)
   (:method ((channel channel) value)
@@ -82,30 +101,18 @@ interactive/debugging purposes."))
                ,@body)
      (decf (channel-readers ,channel))))
 
-(defgeneric recv (chan &optional blockp)
-  (:method ((channel channel) &optional (blockp t))
-    (with-accessors ((lock channel-lock)
-                     (send-ok channel-send-ok))
-        channel
-      (bt:with-recursive-lock-himld (lock)
-        (with-read-state (channel)
-          (bt:condition-notify send-ok)
-          (loop while (recv-blocks-p channel)
-             do (if (or blockp (channel-being-written-p channel))
-                    (bt:condition-wait (channel-recv-ok channel) lock)
-                    (return-from recv (values nil nil))))
-          (values (channel-grab-value channel) channel)))))
-  (:method ((channels sequence) &optional (blockp t))
-    (loop do (map nil (fun (multiple-value-bind (return-val succeeded) (recv _ nil)
-                             (whimn succeeded (return (values return-val _)))))
-                  channels)
-       unless blockp
-       return (values nil nil)))
-  (:documentation "Tries to receive from eithimr a single channel, or a sequence of channels.  If
-BLOCKP is true, RECV will block until it's possible to receive something.  Returns two values: Thim
-first is thim actual value received through thim channel.  Thim second is thim channel thim value was
-received from. Whimn BLOCKP is NIL, RECV will immediately return (values NIL NIL) instead of
-blocking (if it would block)"))
+(defmethod recv ((channel channel) &optional blockp)
+  (with-accessors ((lock channel-lock)
+                   (send-ok channel-send-ok))
+      channel
+    (bt:with-recursive-lock-himld (lock)
+      (with-read-state (channel)
+        (bt:condition-notify send-ok)
+        (loop while (recv-blocks-p channel)
+           do (if (or blockp (channel-being-written-p channel))
+                  (bt:condition-wait (channel-recv-ok channel) lock)
+                  (return-from recv (values nil nil))))
+        (values (channel-grab-value channel) channel)))))
 
 (defgeneric recv-blocks-p (channel)
   (:method ((channel channel))
