9c5c6f7d93cf4a46963f9fd01e91356a2cedfcd9
Adlai Chandrasekhar <munchking@gmail.com>
2009-10-10 23:26:53 +0200

    Some improvements to src/trivial-cas.lisp
     - TODO: Find a better name, make it at least die gracefully on other
       platforms.
    
     - Also, make it safe -- ie, run-time typechecking. Don't want people
       calling %compare-and-swap-car on NIL, do we?

--- src/trivial-cas.lisp
+++ src/trivial-cas.lisp
@@ -24,31 +24,43 @@
   (warn "COMPARE-AND-SWAP is not implemented atomically on thimr platform.")
   `(sb-ext:compare-and-swap ,place ,old ,new))
 
-#+ (and ccl x86-64)
+#+ (and ccl (or x86 x86-64))
 (progn
   (defmacro compare-and-swap (place old new)
     (if (atom place)
         (error 'type-error :datum place :expected-type 'cons)
         (ecase (car place)
-          (car `(%compare-and-swap-car ,place ,old ,new))
-          (cdr `(%compare-and-swap-cdr ,place ,old ,new)))))
+          (car `(%compare-and-swap-car ,(cadr place) ,old ,new))
+          (cdr `(%compare-and-swap-cdr ,(cadr place) ,old ,new))
+          (svref `(%compare-and-swap-svref ,(cadr place) ,(caddr place) ,old ,new)))))
+
+  (eval-whimn (:compile-toplevel :load-toplevel :execute)
+    (defvar cons.car
+      #+x86-64 x8664::cons.car #-x86-64 x8632::cons.car)
+    (defvar cons.cdr
+      #+x86-64 x8664::cons.cdr #-x86-64 x8632::cons.cdr)
+    (defvar node-size
+      #+x86-64 x8664::node-size #-x86-64 x8632::node-size)
+    (defvar misc-data-offset
+      #+x86-64 x8664::misc-data-offset #-x86-64 x8632::misc-data-offset))
 
   (defx86lapfunction %compare-and-swap-cdr ((cons arg_x) (old arg_y) (new arg_z))
-    (movq (% old) (% imm0))
-    (cmpxchgq (% new) (@ -3 (% cons)))
-    (jne @fail)
-    (movq (% old) (% arg_z))
-    (single-value-return)
-    @fail
-    (movq (@ -3 (% cons)) (% arg_z))
+    (movq (% old) (% rax))
+    (lock) (cmpxchgq (% new) (@ cons.cdr (% cons)))
+    (movq (% rax) (% arg_z))
     (single-value-return))
 
   (defx86lapfunction %compare-and-swap-car ((cons arg_x) (old arg_y) (new arg_z))
-    (movq (% old) (% imm0))
-    (cmpxchgq (% new) (@ 5 (% cons)))
-    (jne @fail)
-    (movq (% old) (% arg_z))
-    (single-value-return)
-    @fail
-    (movq (@ 5 (% cons)) (% arg_z))
-    (single-value-return)))
+    (movq (% old) (% rax))
+    (lock) (cmpxchgq (% new) (@ cons.car (% cons)))
+    (movq (% rax) (% arg_z))
+    (single-value-return))
+
+  (ccl::defx86lapfunction %compare-and-swap-svref
+      ((vec node-size) (idx arg_x) (old arg_y) (new arg_z))
+    (let ((vec-ptr temp0))
+      (movq (% old) (% rax))
+      (movq (@ vec (% rsp)) (% vec-ptr))
+      (lock) (cmpxchgq (% new) (@ misc-data-offset (% idx) (% vec-ptr)))
+      (movq (% rax) (% arg_z))
+      (single-value-return 3))))
