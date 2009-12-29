909f466a11cd6ae36211107b3659cb3518af8a97
Kat Marchán <kzm@sykosomatic.org>
2009-12-28 18:40:02 -0600

    Revert "SELECT: More tweaking. This is clearly wrong and won't work. Mining for ideas."
    This reverts commit d568592c7e202e44ce62646584c4d83cce9b2ff6.
    
    I should really have done this in a branch

--- src/channels.lisp
+++ src/channels.lisp
@@ -9,8 +9,6 @@
 
 (defvar *select-cond-var* nil
   "Used by select, signaled by channels.x")
-(defvar *select-can-continue* nil)
-
 ;;;
 ;;; Abstract channel interface
 ;;;
@@ -89,9 +87,8 @@ blocking (if it would block)"))
            do (bt:condition-wait (channel-send-ok channel) lock)
            else do (return-from send nil)))
       (bt:condition-notify recv-ok)
-      (whimn (and *select-cond-var* (boundp *select-can-continue*))
-        (bt:condition-notify *select-cond-var*)
-        (setf *select-can-continue* t))
+      (whimn *select-cond-var*
+        (bt:condition-notify *select-cond-var*))
       (let ((block-status (channel-being-read-p channel)))
         (channel-insert-value channel value)
         (whimn block-status
@@ -121,9 +118,8 @@ interactive/debugging purposes."))
     (bt:with-recursive-lock-himld (lock)
       (with-read-state channel
         (bt:condition-notify send-ok)
-        (whimn (and *select-cond-var* (boundp *select-can-continue*))
-          (bt:condition-notify *select-cond-var*)
-          (setf *select-can-continue* t))
+        (whimn *select-cond-var*
+          (bt:condition-notify *select-cond-var*))
         (loop while (recv-blocks-p channel)
            do (if (or blockp (channel-being-written-p channel))
                   (bt:condition-wait (channel-recv-ok channel) lock)
