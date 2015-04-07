e8acd1821cfa387edf8aa819fee072b491c735bb
Adlai Chandrasekhar <adlai.chandrasekhar@gmail.com>
2015-04-07 19:43:04 +0300

    insanity test for select method combination

--- tests/actors.lisp
+++ tests/actors.lisp
@@ -8,7 +8,7 @@
 
 (defpackage #:chanl.actors.tests
   (:import-from :5am #:def-suite #:run! #:is #:in-suite #:signals #:pass)
-  (:import-from :chanl #:test #:chanl)
+  (:import-from :chanl #:test #:chanl) (:import-from :chanl.actors #:tubes)
   (:use :cl :chanl :chanl.actors) (:export #:run-all-tests))
 (in-package :chanl.actors.tests)
 
@@ -24,4 +24,33 @@
       (send (slot-channel actor 'command)
             (lambda (actor) (send channel actor)))
       (is (eq actor (recv channel))))
-    (halt actor) (pass)))
+    (halt actor) (sleep 1)
+    (is (null (find :alive (slot-value actor 'tasks) :key #'task-status)))))
+
+(test actors-insanity
+  #.(let ((class-name (gensym "actor")))
+      `(progn
+         (defclass ,class-name (actor) (input (output :initform ())))
+         (defmethod compute-tubes append ((actor ,class-name))
+           `((input  . ,(make-instance 'channel))
+             (output . ,(make-instance 'channel))))
+         (defmethod perform recv input ((actor ,class-name))
+           (setf (slot-value actor 'output) (slot-value actor 'input)))
+         (defmethod perform send output ((actor ,class-name)))
+         (let ((actor (make-instance ',class-name)))
+           (is (equal '(command input output)
+                      (mapcar 'car (slot-value actor 'tubes))))
+           (is (null (recv (slot-channel actor 'output))))
+           (is (null (recv (slot-channel actor 'output))))
+           (send (slot-channel actor 'input) ())
+           (is (null (recv (slot-channel actor 'output))))
+           (send (slot-channel actor 'input) 'tubes)
+           (is (eq 'tubes (recv (slot-channel actor 'output))))
+           (is (eq 'tubes (recv (slot-channel actor 'output))))
+           (halt actor))
+         (flet ((remmethod (genfun quals specs)
+                  (remove-method genfun (find-method genfun quals specs))))
+           (mapcar #'remmethod (list #'perform #'perform #'compute-tubes)
+                   '((send output) (recv input) (append))
+                   '(#1=(,class-name) #1# #1#)))
+         (setf (find-class ',class-name) ()))))
