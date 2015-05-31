c99ab1a3895bb5ddad6666afafbf9b79a38e78ca
Adlai <adlai@users.noreply.github.com>
2015-06-01 02:29:41 +0300

    Delete examples.lisp
    It's not even in #<ASDF:SYSTEM CHANL.TESTS>

--- tests/examples.lisp
+++
@@ -1,7 +0,0 @@
-;;;; -*- Mode: Lisp; Syntax: ANSI-Common-Lisp; Base: 10; indent-tabs-mode: nil -*-
-;;;;
-;;;; Copyright © 2009 Kat Marchan, Adlai Chandrasekhar
-;;;;
-;;;; Tests for example code
-;;;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
